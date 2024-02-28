413a8e7ba89fb0c6985e74f2ff1858a6f6d8aa81
Anne201 <118590269+Anne201@users.noreply.github.com>
2024-02-28 11:18:24 +0800

    match_menu_set.js

--- multi04/match_menu_set.js
+++ multi04/match_menu_set.js
@@ -1,9 +1,9 @@
 menu_title='對對碰';
 menu_items = new Array(
-  '一年級對對碰-1,match_set_1.js'
-, '二年級對對碰-2,match_set_2.js'
-, '三年級對對碰-3,match_set_3.js'
-, '四年級對對碰-4,match_set_4.js'
-, '五年級對對碰-5,match_set_5.js'
-, '六年級對對碰-6,match_set_6.js'
+  '一年級對對碰,match_set_1.js'
+, '二年級對對碰,match_set_2.js'
+, '三年級對對碰,match_set_3.js'
+, '四年級對對碰,match_set_4.js'
+, '五年級對對碰,match_set_5.js'
+, '六年級對對碰,match_set_6.js'
 );
